bad05a905069394c2eafaa707169e4092f0da349
frcalan <frcalan@gmail.com>
2023-10-25 22:41:22 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/io/github/frcalan/helpdesk/domain/Cliente.java
+++ src/main/java/io/github/frcalan/helpdesk/domain/Cliente.java
@@ -1,16 +1,25 @@
 package io.github.frcalan.helpdesk.domain;
 
+import io.github.frcalan.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Pessoa {
+    private static final long serialVersionUID = 1L;
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
+        addPefis(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
         super(id, nome, cpf, email, senha);
+        addPefis(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
--- src/main/java/io/github/frcalan/helpdesk/domain/Tecnico.java
+++ src/main/java/io/github/frcalan/helpdesk/domain/Tecnico.java
@@ -1,16 +1,24 @@
 package io.github.frcalan.helpdesk.domain;
 
+import io.github.frcalan.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
-
+        addPefis(Perfil.TECNICO);
     }
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPefis(Perfil.TECNICO);
     }
 
     public List<Chamado> getChamados() {
