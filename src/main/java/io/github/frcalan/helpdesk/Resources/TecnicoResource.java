9a6195b1a6759de886c0ea6d2f76a51399218d6b
frcalan <frcalan@gmail.com>
2023-11-18 20:35:23 -0300

    Padrão DTO para Tecnico

--- src/main/java/io/github/frcalan/helpdesk/Resources/TecnicoResource.java
+++ src/main/java/io/github/frcalan/helpdesk/Resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package io.github.frcalan.helpdesk.Resources;
 
 import io.github.frcalan.helpdesk.domain.Tecnico;
+import io.github.frcalan.helpdesk.domain.dtos.TecnicoDTO;
 import io.github.frcalan.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,8 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 }
