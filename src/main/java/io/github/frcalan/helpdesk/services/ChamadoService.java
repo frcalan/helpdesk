75c6be9768e247efc2ffb9e866aff3e08195d62e
frcalan <frcalan@gmail.com>
2023-11-22 16:15:57 -0300

    findById para Chamado

---
+++ src/main/java/io/github/frcalan/helpdesk/services/ChamadoService.java
@@ -0,0 +1,18 @@
+package io.github.frcalan.helpdesk.services;
+
+import io.github.frcalan.helpdesk.domain.Chamado;
+import io.github.frcalan.helpdesk.repositories.ChamadoRepository;
+import io.github.frcalan.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ChamadoService {
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado FindById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ID: " + id));
+    }
+}
