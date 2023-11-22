ece135cd1c53b4b2c62cb94e757c4ae123cb4f2f
frcalan <frcalan@gmail.com>
2023-11-22 16:22:52 -0300

    findAll para Chamado

--- src/main/java/io/github/frcalan/helpdesk/services/ChamadoService.java
+++ src/main/java/io/github/frcalan/helpdesk/services/ChamadoService.java
@@ -5,6 +5,7 @@ import io.github.frcalan.helpdesk.repositories.ChamadoRepository;
 import io.github.frcalan.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ChamadoService {
@@ -14,5 +15,9 @@ public class ChamadoService {
     public Chamado FindById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ID: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
     }
 }
