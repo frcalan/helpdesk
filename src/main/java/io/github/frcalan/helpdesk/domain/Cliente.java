e0720460f930be86a1fa6138ff3a02fc4fa80e63
frcalan <frcalan@gmail.com>
2023-11-22 15:34:18 -0300

    CRUD para Cliente implementado

--- src/main/java/io/github/frcalan/helpdesk/domain/Cliente.java
+++ src/main/java/io/github/frcalan/helpdesk/domain/Cliente.java
@@ -1,12 +1,14 @@
 package io.github.frcalan.helpdesk.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.frcalan.helpdesk.domain.dtos.ClienteDTO;
 import io.github.frcalan.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa {
@@ -22,6 +24,16 @@ public class Cliente extends Pessoa {
     public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
         super(id, nome, cpf, email, senha);
         addPefis(Perfil.CLIENTE);
+    }
+
+    public Cliente(ClienteDTO obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
     }
 
     public List<Chamado> getChamados() {
